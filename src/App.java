6e028a8dc1af20806a81072629c3af2d9b5c7a72
Naggas07 <rober.araya.gil@gmail.com>
2020-07-06 13:42:45 +0200

    primitives variables in java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!, How are you?"); // coment de una linea
+        /*
+         * coment de varias lienas
+         * 
+         */
+
+        // variables
+
+        byte entero = 127;
+        short entero2 = 32767;
+        int entero3 = 2000000000;
+
+        float decimal = 3.45f;
+
+        char caracter = 'a';
+
+        boolean decision = false;
+
+        System.out.println("variable a imprimir = " + decision);
     }
 }
