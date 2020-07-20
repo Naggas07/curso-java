089d45e00cc971d64cd99ed671d75630d8a401b5
Naggas07 <rober.araya.gil@gmail.com>
2020-07-20 15:40:38 +0200

    while and do while

---
+++ src/bucles.java
@@ -0,0 +1,19 @@
+public class bucles {
+    public static void main(String[] args) {
+        int num = 0;
+
+        while (num <= 10) {
+            System.out.println(num);
+            num++;
+        }
+
+        num = 0;
+        do {
+            System.out.println(num);
+            num++;
+        } while (num <= 10);
+
+        num = 0;
+
+    }
+}
