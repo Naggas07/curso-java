0f85022d1721a89538c351a8913f21063f0a624d
Naggas07 <rober.araya.gil@gmail.com>
2020-07-22 19:02:12 +0200

    done second definition array

--- src/arrays.java
+++ src/arrays.java
@@ -6,7 +6,10 @@ public class arrays {
         numeros[1] = 2;
         numeros[2] = 3;
 
+        int[] numeros2 = { 1, 2, 3, 4 };
+
         System.out.println(numeros[2]);
+        System.out.println(numeros2[3]);
 
     }
 }
